9126387d494078eef87c2d6360c17e656cca71e3
progof <progof031@gmail.com>
2018-08-10 19:38:41 +0300

    Update file

--- src/alert.cpp
+++ src/alert.cpp
@@ -20,7 +20,7 @@ map<uint256, CAlert> mapAlerts;
 CCriticalSection cs_mapAlerts;
 
 static const char* pszMainKey = "040184710fa689ad5023690c80f3a49c8f13f8d45b8c857fbcbc8bc4a8e4d3eb4b10f4d4604fa08dce601aaf0f470216fe1b51850b4acf21b179c45070ac7b03a9";
-static const char* pszTestKey = "04302390343f91cc401d56d68b123028bf52e5fca1939df127f63c6467cdf9c8e2c14b61104cf817d0b780da337893ecc4aaff1309e536162dabbdb45200ca2b0a";
+static const char* pszTestKey = "0450d063c6ef6d318bad4b5d1b435acdf69730d69f0c6775fb945a86334b1a24255e87a897207cc6b3d08200999f343c6f904ab6d9528de4ed5d5eb3754d5096ae";
 
 void CUnsignedAlert::SetNull()
 {
